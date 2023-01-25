e7c0a51714b6130a9c84a66707a695db9f17fdfc
pavel.savelev <pal.igory4@gmail.com>
2023-01-25 16:23:32 +0300

    Table collecting success

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# https://qna.habr.com/q/480867
+
 import requests, gc
 from bs4 import BeautifulSoup
 
@@ -5,7 +7,6 @@ headers = {'Accept': '*/*', 'Connection': 'keep-alive', 'User-Agent': 'Mozilla/5
 
 def getRunnersData(dikiye_url = 'https://app.dikiye.ru/statistics/',
                   number_of_lines=2000):
-    """Сбор информации о действующих экспертах"""
     global experts_quantity
     runners_quantity = number_of_lines
     response = requests.get(dikiye_url, headers=headers)
@@ -14,43 +15,20 @@ def getRunnersData(dikiye_url = 'https://app.dikiye.ru/statistics/',
     gc.collect()
     soup = soup.find('table')
 
-    t_headers = {'Трейлраннер', 'Количество трейлов', 'Количество уникальных локаций', 'Общая дистанция',
-                 'Пейсмейкер или замыкающий', 'Бежал с собакой', 'Привел друга на трейл',
-                 'Присматривал за детьми на трейле'}
     rows = soup.find_all("tr")
 
-    data = []
+    table_headers = []
+    data = list()
 
     for row in rows:
+        runner = list()
         cells = row.find_all("td")
-        print(cells)
-    # item = {}
-    #
-    # for index in t_headers:
-    #     item[headers[index]] = cells[index].text
-    #     data.append(item)
+        for i in range(len(cells)):
+            runner.append(str(cells[i]).lstrip('<td>').rstrip('</td>'))
+        data.append(runner)
+    data.remove([])
 
-    # search_lines = ['tr-'+str(i) for i in range(1, number_of_lines + 1)]
-    # line1 = soup.find('tbody')
-    # del soup
-    # gc.collect()
-    #
-    # for _ in search_lines:
-    #     line2 = line1.find(class_=_)
-    #     if line2 == None:
-    #         experts_quantity = search_lines.index(_)
-    #         break
-    #     name = line2.find(class_='td-0').text
-    #     date_since = line2.find(class_='td-1').text
-    #     date_until = line2.find(class_='td-2').text
-    #     sphere = line2.find(class_='td-3').text
-    #     okpd_codes = line2.find(class_='td-4').text
-    #     department = line2.find(class_='td-5').text
-    #     extra = line2.find(class_='td-6').text
-    #     yield name,date_since, date_until, sphere, okpd_codes, department, extra
-    #
-    # del line2, search_lines, name, date_since, date_until, sphere, okpd_codes, department, extra
-    # gc.collect()
     return data
 if __name__ == '__main__':
-    print(getRunnersData())
+    for i in range(len(getRunnersData())):
+        print(getRunnersData())

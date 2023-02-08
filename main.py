9f69f00bcd5980fe30e8888124600a560c159288
paligory4 <pal.igory4@gmail.com>
2023-02-08 16:29:53 +0300

    criteria added

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from getRunnersData import table_headers, getRunnersData
 
 table = list(getRunnersData())
-for j in range(len(table)):
+table_filtered = []
+margins = ['8','9','28','29','58','59','98','99']
+for j, _ in enumerate(table):
+    if table[j]['Пейсмейкер или замыкающий'] in margins:
+        table_filtered.append(table[j])
+
+for j, _ in enumerate(table_filtered):
     for i in table_headers:
-        print(table[j][i], end=' ')
+        print(table_filtered[j][i], end=' ')
     print()

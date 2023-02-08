2fd1e9ce7c0b1514e79584b920253c4b00a0027f
pavel-savelev <pal.igory4@gmail.com>
2023-02-08 17:54:57 +0300

    refactored

--- main.py
+++ main.py
@@ -1,19 +1,44 @@
-import xlsxwriter
+import xlsxwriter, requests
 from datetime import date
-from getRunnersData import table_headers, getRunnersData
+from bs4 import BeautifulSoup
+
+headers = {'Accept': '*/*', 'Connection': 'keep-alive',
+           'User-Agent': 'Mozilla/5.0 (Windows NT 6.0) AppleWebKit/537.36 (KHTML, like Gecko) '
+                         'Chrome/70.0.3538.67 Safari/537.36 OPR/56.0.3051.104'}
+
+table_headers = ['№', 'Трейлраннер', 'Количество трейлов', 'Количество уникальных локаций',
+                 'Общая дистанция', 'Пейсмейкер или замыкающий', 'Бежал с собакой', 'Привел друга на трейл',
+                 'Присматривал за детьми на трейле', 'Баллы']
+dikiye_url = 'https://app.dikiye.ru/statistics/'
+
+def getRunnersData():
+    response = requests.get(dikiye_url, headers=headers)
+    soup = BeautifulSoup(response.text, 'lxml')
+    del response
+    soup = soup.find('table')
+    rows = soup.find_all("tr")
+    data = list()
+    for row in rows:
+        runner = dict()
+        cells = row.find_all("td")
+        for j in range(len(cells)):
+            runner[table_headers[j]] = (str(cells[j]).lstrip('<td>').rstrip('</td>'))
+        data.append(runner)
+    data.remove({})
+    return data
 
 def xls_table(pacer_table):
-    today = str(date.today)
-    book = xlsxwriter.Workbook('Награждения ред.' + today + '.xlsx')
-    page = book.add_worksheet('Награждения')
+    today = str(date.today())
+    book = xlsxwriter.Workbook(f'Marshals {today}.xlsx')
+    page = book.add_worksheet('Marshals')
 
-    page.set_column('A:A', 10)
-    page.set_column('B:B', 40)
-    page.set_column('C:C', 10)
+    page.set_column('A:A', 4)
+    page.set_column('B:B', 22)
+    page.set_column('C:C', 5)
 
     page.write(0,0,'№')
     page.write(0,1,'ФИО')
-    page.write(0,2,'Кол-во пейсов')
+    page.write(0,2,'Пейс')
     row = 1
     column = 0
     for i,_ in enumerate(pacer_table):
@@ -22,7 +47,7 @@ def xls_table(pacer_table):
         page.write(row, column + 1, name)
         page.write(row, column + 2, paces)
         row += 1
-    book.close
+    book.close()
 
 table = list(getRunnersData())
 table_filtered = []
